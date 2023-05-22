674e01853e6e5489f9123c5590cfe38ce6b5f9be
VK <job@b-vladi.ru>
2023-05-23 02:59:52 +0300

    Source

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import router from '@/router/index.js'
+import App from '@/App.vue'
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
