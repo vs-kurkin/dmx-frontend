3ed1f1311e13f2c01e056f5b716c228e7f41ace4
VK <job@b-vladi.ru>
2023-05-23 10:25:38 +0300

    Added UI component library

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import './assets/main.css'
+
+import 'primevue/resources/themes/viva-dark/theme.css'
+import 'primevue/resources/primevue.min.css'
+
 import { createApp } from 'vue'
-import router from '@/router/index.js'
+import PrimeVue from 'primevue/config'
+import router from  '@/router/index.js'
 import App from '@/App.vue'
 
 const app = createApp(App)
+
 app.use(router)
+app.use(PrimeVue)
+
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import Button from 'primevue/button';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Button label="Count" />
   </main>
 </template>
