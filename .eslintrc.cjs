9fd723ec883a4f1728024ec355f7f759409b96c0
VK <job@b-vladi.ru>
2023-05-23 10:24:52 +0300

    Fix TypeScript configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,22 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  root: true,
-  'extends': [
-    'prettier',
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
   env: {
     node: true,
-    es6: true
+    es6: true,
+    browser: true
   },
+  "extends": [
+    "prettier",
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting"
+  ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  }
-}
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  root: true
+};
